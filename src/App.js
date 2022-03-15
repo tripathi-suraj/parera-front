9bc2b27f0092882b63d69e899b25f575293431cf
tripathi-suraj <surajtripathi781@gmail.com>
2022-03-15 19:51:50 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
