e929c48cffe61398c0a5a1ce50c251da2174f758
tripathi-suraj <surajtripathi781@gmail.com>
2022-03-15 09:22:11 +0530

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Headers from './common/Header';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <>
+      <Headers />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-    </div>
+    </>
   );
 }
---
+++ src/common/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Headers = () => {
+  return (
+    <>
+      <h1> Hello </h1>;
+    </>
+  );
+};
+
+export default Headers;
